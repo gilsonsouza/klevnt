8710026f36355d75f4be75cbb208acbde6473140
Renne Silva <rennedasilva@hotmail.com>
2016-06-04 03:02:44 +0200

    Search engine

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -24,13 +24,23 @@ angular.module('app.controllers', [])
 
 .controller('cultureCtrl', function($scope,$state, CultureEvents, DetailsCulture) {
   $scope.cultureEvents = CultureEvents;
-  $scope.getDatetime = new Date();
-  console.log($scope.getDatetime);
+  $scope.today = new Date();
+
   $scope.setDetails = function(culture)
   {
     DetailsCulture.set(culture);
     $state.go('tabsController.page10');
   };
+  
+  $scope.filterDate = function(){
+    return function(item){
+      date = new Date();
+      date.setFullYear(item["year"],item["month"]-1,item["day"]);
+      date.setHours(0,0,0,0,0);
+      $scope.today.setHours(0,0,0,0,0);
+      return (date.getTime() == $scope.today.getTime());
+    }
+  }
 })
 
 .controller('page5Ctrl', function($scope, DetailsParty) {
@@ -50,8 +60,26 @@ angular.module('app.controllers', [])
 
 })
 .controller('allPartiesCtrl', function($scope, $state, Parties, DetailsParty) {
+
   $scope.parties = Parties;
-  date = new Date();
+
+  today = new Date();
+  today.setHours(0,0,0,0,0);
+  var i = 0;
+
+  for( var x in $scope.parties) {
+    date = new Date();
+    date.setFullYear($scope.parties[x].year,$scope.parties[x].month-1,$scope.parties[x].day);
+    date.setHours(0,0,0,0,0);
+    if(date < today)
+      i++;
+  }
+
+  var total = $scope.parties.length - i;
+
+  $scope.filteredItems = total; //Initially for no filter  
+  $scope.totalItems = total;
+
   $scope.setDetails = function(party)
   {
     DetailsParty.set(party);
@@ -59,15 +87,39 @@ angular.module('app.controllers', [])
   };
   $scope.filterDate = function(){
     return function(item){
-      return ((item["year"] == date.getFullYear() && item["month"] > date.getMonth()
-        && item["day"] >= date.getDate()) || item["year"] > date.getFullYear());
+      date = new Date();
+      date.setFullYear(item["year"],item["month"]-1,item["day"]);
+      date.setHours(0,0,0,0,0);
+      return (date >= today);
     }
   }
+
+  $scope.filter = function() {
+    $timeout(function() { 
+      $scope.filteredItems = $scope.filtered.length;
+    }, 10);
+  };
 })
 
 .controller('allSportsCtrl', function($scope, $state, Sports, DetailsSport) {
   $scope.sports = Sports;
-  date = new Date();
+  today = new Date();
+  today.setHours(0,0,0,0,0);
+  var i = 0;
+
+  for( var x in $scope.sports) {
+    date = new Date();
+    date.setFullYear($scope.sports[x].year,$scope.sports[x].month-1,$scope.sports[x].day);
+    date.setHours(0,0,0,0,0);
+    if(date < today)
+      i++;
+  }
+
+  var total = $scope.sports.length - i;
+
+  $scope.filteredItems = total; //Initially for no filter  
+  $scope.totalItems = total;
+
   $scope.setDetails = function(sport)
   {
     DetailsSport.set(sport);
@@ -75,15 +127,39 @@ angular.module('app.controllers', [])
   };
   $scope.filterDate = function(){
     return function(item){
-      return ((item["year"] == date.getFullYear() && item["month"] > date.getMonth()
-        && item["day"] >= date.getDate()) || item["year"] > date.getFullYear());
+      date = new Date();
+      date.setFullYear(item["year"],item["month"]-1,item["day"]);
+      date.setHours(0,0,0,0,0);
+      return (date >= today);
     }
   }
+
+  $scope.filter = function() {
+    $timeout(function() { 
+      $scope.filteredItems = $scope.filtered.length;
+    }, 10);
+  };
 })
 
 .controller('allCultureCtrl', function($scope, $state, CultureEvents, DetailsCulture) {
   $scope.cultureEvents = CultureEvents;
-  date = new Date();
+  today = new Date();
+  today.setHours(0,0,0,0,0);
+  var i = 0;
+
+  for( var x in $scope.cultureEvents) {
+    date = new Date();
+    date.setFullYear($scope.cultureEvents[x].year,$scope.cultureEvents[x].month-1,$scope.cultureEvents[x].day);
+    date.setHours(0,0,0,0,0);
+    if(date < today)
+      i++;
+  }
+
+  var total = $scope.cultureEvents.length - i;
+
+  $scope.filteredItems = total; //Initially for no filter  
+  $scope.totalItems = total;
+
   $scope.setDetails = function(cultureEvent)
   {
     DetailsCulture.set(cultureEvent);
@@ -91,9 +167,16 @@ angular.module('app.controllers', [])
   };
   $scope.filterDate = function(){
     return function(item){
-      return ((item["year"] == date.getFullYear() && item["month"] > date.getMonth()
-        && item["day"] >= date.getDate()) || item["year"] > date.getFullYear());
+      date = new Date();
+      date.setFullYear(item["year"],item["month"]-1,item["day"]);
+      date.setHours(0,0,0,0,0);
+      return (date >= today);
     }
   }
 
+  $scope.filter = function() {
+    $timeout(function() { 
+      $scope.filteredItems = $scope.filtered.length;
+    }, 10);
+  };
 })
